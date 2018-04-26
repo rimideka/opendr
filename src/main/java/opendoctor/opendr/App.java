52e8180c0d0969df6aae2ce8132c23eb9a3c1da4
HPUSER <HPUSER@HP-PC>
2018-04-26 23:46:30 +0530

    commitment for opendr

---
+++ src/main/java/opendoctor/opendr/App.java
@@ -0,0 +1,14 @@
+package opendoctor.opendr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Hiiiiiiii World!" );
+    }
+}
